55085867d760c6c69dea8d71bb7577dd89e5867f
Fridtjof-DE <fridtjof.goes@gmx.de>
2020-07-10 14:26:01 +0200

    Init

--- src/main/java/tk/fridtjof/pott/ConfigManager.java
+++ src/main/java/tk/fridtjof/pott/ConfigManager.java
@@ -17,7 +17,7 @@ public class ConfigManager {
     private void setDefaults() {
         logger.info("Settings configuration defaults...");
         config.setDefault("license", false);
-        config.setDefault("token", "NzIwMzgxMTk3NjQ5NDQ1MDE0.XuFJSg.NI4T06SjlW8pT3AbJlEVDu0E0YE");
+        config.setDefault("token", "your token");
         config.setDefault("motd", "over the databases");
         config.setDefault("prefix", "!");
         config.setDefault("icon", "https://cdn.discordapp.com/attachments/475679464228126721/697921218560393276/sheep.gif");
