b064e2f5bd0111c5d6092fb0e5a42dc45ea8afc3
Fridtjof_DE <38737231+Fridtjof-DE@users.noreply.github.com>
2021-07-09 10:19:27 +0200

    Create HeadCmd.java

---
+++ src/main/java/tk/fridtjof/pott/cmds/HeadCmd.java
@@ -0,0 +1,47 @@
+package tk.fridtjof.pott.cmds;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import tk.fridtjof.pott.Pott;
+import tk.fridtjof.puddingapi.general.utils.MojangAPI;
+import tk.fridtjof.puddingapi.jda.GuildCommand;
+
+import java.awt.*;
+
+public class HeadCmd extends GuildCommand {
+
+    public HeadCmd(String commandName, GuildMessageReceivedEvent event) {
+        super(commandName, event);
+    }
+
+    @Override
+    public void logic() {
+        Pott pott = Pott.getInstance();
+        String p = pott.config.getString("prefix");
+
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setColor(Color.GREEN);
+        embedBuilder.setFooter("Pott by Fridtjof_DE#2605");
+
+        if (arguments().length == 1) {
+            if (arguments()[0].equalsIgnoreCase(p + "head")) {
+                event.getMessage().addReaction("❌").queue();
+                event.getChannel().sendMessage("**Please enter a name!**").queue();
+            }
+        }
+
+        if (arguments().length == 2) {
+            if (arguments()[0].equalsIgnoreCase( p + "head")) {
+                event.getMessage().addReaction("✔️").queue();
+                String uuid = MojangAPI.getUuidFromUsername(arguments()[1]);
+                if(uuid == null) {
+                    event.getChannel().sendMessage("**Error!**").queue();
+                } else {
+                    embedBuilder.setImage("https://mc-heads.net/head/" + uuid + "/128.png");
+                    embedBuilder.setAuthor("Head render of " + arguments()[1], "https://discord.gg/XzZZAme", pott.config.getString("icon"));
+                    event.getChannel().sendMessage(embedBuilder.build()).queue();
+                }
+            }
+        }
+    }
+}
