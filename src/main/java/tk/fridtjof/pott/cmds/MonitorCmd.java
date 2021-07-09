6142cec9e289839291ab64831fe41c0a1ae6749d
Fridtjof_DE <38737231+Fridtjof-DE@users.noreply.github.com>
2021-07-09 10:19:29 +0200

    Create MonitorCmd.java

---
+++ src/main/java/tk/fridtjof/pott/cmds/MonitorCmd.java
@@ -0,0 +1,24 @@
+package tk.fridtjof.pott.cmds;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import tk.fridtjof.puddingapi.jda.GuildCommand;
+
+public class MonitorCmd extends GuildCommand {
+
+    public MonitorCmd(String commandName, GuildMessageReceivedEvent event) {
+        super(commandName, event);
+    }
+
+    @Override
+    public void logic() {
+        if(arguments().length == 1) {
+            Runtime runtime = Runtime.getRuntime();
+            long memTotal = runtime.totalMemory() / 1048576;
+            long memFree = runtime.freeMemory() / 1048576;
+            long memUsed = memTotal - memFree;
+            double memPercent = (double) memUsed / (double) memTotal * 100;
+
+            event.getChannel().sendMessage("Memory usage: `" + (int) memPercent + "%` - `" + memUsed + "mb`/`" + memTotal + "mb` - `" + memFree + "mb` free!").queue();
+        }
+    }
+}
