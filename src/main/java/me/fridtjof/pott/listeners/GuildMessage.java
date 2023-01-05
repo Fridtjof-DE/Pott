c9b4a1f098e094ab0ae60bd80cff577f136cd745
Fridtjof-DE <fridtjof.goes@gmx.de>
2023-01-05 13:14:24 +0100

    JDA update  + counting

--- src/main/java/me/fridtjof/pott/listeners/GuildMessage.java
+++ src/main/java/me/fridtjof/pott/listeners/GuildMessage.java
@@ -1,10 +1,7 @@
 package me.fridtjof.pott.listeners;
 
 import me.fridtjof.pott.Pott;
-import me.fridtjof.pott.cmds.HeadCmd;
-import me.fridtjof.pott.cmds.MonitorCmd;
-import me.fridtjof.pott.cmds.PingCmd;
-import me.fridtjof.pott.cmds.UuidCmd;
+import me.fridtjof.pott.cmds.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.slf4j.Logger;
@@ -17,13 +14,17 @@ public class GuildMessage extends ListenerAdapter {
     static Pott pott = Pott.getInstance();
     String p = pott.config.getString("prefix");
 
+    Counting counting = new Counting();
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
 
-        new PingCmd(p +"ping", event);
-        new HeadCmd(p +"head", event);
-        new UuidCmd(p +"uuid", event);
-        new MonitorCmd(p +"monitor", event);
+        new PingCmd(p + "ping", event);
+        new HeadCmd(p + "head", event);
+        new UuidCmd(p + "uuid", event);
+        new MonitorCmd(p + "monitor", event);
+        new MonitorCmd(p + "monitor", event);
+        counting.count(event);
 
         //SPAM
         if (event.getMessage().getContentRaw().equalsIgnoreCase("ja")) {
@@ -32,6 +33,7 @@ public class GuildMessage extends ListenerAdapter {
             event.getChannel().sendMessage("doch").queue();
         } else if (event.getMessage().getContentRaw().equalsIgnoreCase("bier") || event.getMessage().getContentRaw().equalsIgnoreCase("beer")) {
             event.getChannel().sendMessage("||https://www.brauerei-allersheim.de/||").queue();
+            System.out.println("BIER");
         }
     }
 }
